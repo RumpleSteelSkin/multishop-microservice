19580ff9e60a3d5fbb9dfc7cb1a0c8ed4790dbe5
RumpleSteelSkin <osmnistbayrak@gmail.com>
2025-08-07 18:55:39 +0300

    feat(security): integrate IdentityServer 3.1 for authentication

---
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Controller/OrderingsController.cs
@@ -0,0 +1,41 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
+using MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+
+namespace MultiShop.Order.WebApi.Controller;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderingsController(IMediator mediator) : ControllerBase
+{
+    [HttpGet("GetAll")]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await mediator.Send(new GetOrderingQuery()));
+    }
+
+    [HttpGet("GetById/{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        return Ok(await mediator.Send(new GetOrderingByIdQuery { Id = id }));
+    }
+
+    [HttpPost("Create")]
+    public async Task<IActionResult> Create([FromBody] CreateOrderingCommand command)
+    {
+        return Ok(await mediator.Send(command));
+    }
+
+    [HttpPut("Update")]
+    public async Task<IActionResult> Update([FromBody] UpdateOrderingCommand command)
+    {
+        return Ok(await mediator.Send(command));
+    }
+
+    [HttpDelete("Delete/{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        return Ok(await mediator.Send(new RemoveOrderingCommand(id)));
+    }
+}
