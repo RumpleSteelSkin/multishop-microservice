1cab8ffec2028baa4eb08fe91062258ca759a0e3
RumpleSteelSkin <osmnistbayrak@gmail.com>
2025-08-11 15:35:43 +0300

    Created Contact controller and refactored pages into ViewComponents.

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ContactViewComponents/_DetailContactComponentPartial.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ContactViewComponents;
+
+public class _DetailContactComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
