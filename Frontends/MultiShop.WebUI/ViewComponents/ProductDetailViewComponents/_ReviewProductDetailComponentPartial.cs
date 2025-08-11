056a0239645a2401222cc3cf063fc340ac0169c6
RumpleSteelSkin <osmnistbayrak@gmail.com>
2025-08-11 13:09:24 +0300

    Created ProductDetail controller and refactored pages into ViewComponents.

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ReviewProductDetailComponentPartial.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents;
+
+public class _ReviewProductDetailComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
