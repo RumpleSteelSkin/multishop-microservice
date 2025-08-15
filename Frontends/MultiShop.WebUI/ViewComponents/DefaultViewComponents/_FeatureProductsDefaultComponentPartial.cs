82eee0fe6d1ae65ef4f96784c79bfc3fe64a3ac0
RumpleSteelSkin <osmnistbayrak@gmail.com>
2025-08-15 20:38:09 +0300

    Consumed featured products API for homepage display.

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using MultiShop.WebUI.Constant;
+using MultiShop.WebUI.Hooks;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents;
 
-public class _FeatureProductsDefaultComponentPartial : ViewComponent
+public class _FeatureProductsDefaultComponentPartial(JsonService jsonService) : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    public async Task<IViewComponentResult> InvokeAsync()
     {
-        return View();
+        return View(await jsonService.GetAllAsync<ResultProductDto>(ApiRoutes.Products.GetAll));
     }
 }
